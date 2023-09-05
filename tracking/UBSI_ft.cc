f143c35699dadff0de4ec8ac3926baec7343ee59
Basanta Chaulagain <basanta83chaulagain@gmail.com>
2023-09-05 15:23:54 -0400

    fixed forward tracking

--- tracking/UBSI_ft.cc
+++ tracking/UBSI_ft.cc
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
+#include <stack>
 #include <sstream>
 #include <chrono>
 #include <ctime>
 #include <stdlib.h>
-#include <Python.h>
 #include "init_scan.h"
 #include "utils.h"
 #include "tables.h"
@@ -208,246 +209,75 @@ void ft_syscall_handler(char * buf, double ts, double* forward_ts, int* ret_pid,
 		}
 }
 
-void table_scan_f(int user_pid, long user_inode)
-{
-	#ifdef GET_STATS
-		int count_call_to_server = 0;
-		chrono::duration<double> runtime[1000] = {};
-		int lines_in_buf[1000];
-		int total_logs[1000];
-	#endif
+void table_scan_f(int user_pid, long user_inode, FILE *fp){
+	char buf[4096];
+	int max_log_len = 500;
+	int first_iteration = 1;
+
+	long kw;
+	if (user_pid>0) kw = long(user_pid);
+	else if (user_inode>0) kw = user_inode;
+
+	forward_ts = 0;
+	char line[4096];
 	
-		int max_log_len = 500;
-		char* buf = (char*) malloc((200000*max_log_len) * sizeof(char));
-		// char buf[16000][500];
-		// int eid_list[10000], eid_index=0;
-		long keywords[1000] = {0};
-		string next_keyword;
-		int i, k, start_index = 0, stop_index, first_iteration = 1;
-		int buf_add_index, keyword_add_index, keyword_search_index;
-		buf_add_index = keyword_add_index = keyword_search_index = 0;
-	
-		if (user_pid>0) keywords[0] = long(user_pid);
-		else if (user_inode>0) keywords[0] = user_inode;
-				
-		PyObject *pName, *pModule, *pFunc, *pArg, *pValue;
-		Py_Initialize();
-		PyRun_SimpleString("import os, sys, inspect\n"
-							"parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))\n"
-							"sys.path.insert(0, parent_dir)\n");
-		pName = PyUnicode_DecodeFSDefault("client.client");
-		pModule = PyImport_Import(pName);
-		Py_DECREF(pName);
-		if (pModule == NULL){
-			PyErr_Print();
-			return;
+	while (fgets(line, sizeof(line), fp) != NULL) {
+		if (strtol(line, NULL, 10) == 0){
+			continue;
 		}
-		pFunc = PyObject_GetAttrString(pModule, "sse_search");
-		if (pFunc==NULL || !PyCallable_Check(pFunc)){
-			printf("Error initializing function.\n");
-			Py_DECREF(pModule);
-			return;
+		char temp[4096], *ptr;
+		strcpy(temp, line);
+		ptr = strtok(temp, ";");
+		ptr = strtok(NULL, ";");
+		ptr = strtok(NULL, ";");
+		if (strncmp(ptr, " UBSI_ENTRY", 11)==0 || strncmp(ptr, " UBSI_EXIT", 10)==0 || strncmp(ptr, " UBSI_DEP", 9)==0){
+			continue;
+		}
+		ptr = strtok(ptr, "(");
+		ptr = strtok(NULL, ")");
+		int sysno = atoi(ptr);
+		if (is_file_create(sysno)==0 && is_exec(sysno)==0 && is_write(sysno)==0 && is_read(sysno)==0 && is_fork_or_clone(sysno)==0){
+			continue;
 		}
 		
-		forward_ts = 0;
-		char* search_type = "f";
-		do {
-			#ifdef GET_STATS
-				auto loop_start_ts = chrono::system_clock::now();
-				int total_log_lines = 0;
-				long double meta_data[4];
-				int meta_idx=0;
-				// auto c1_send_ts_ = chrono::system_clock::now().time_since_epoch().count();
-				// long double c1_send_ts = (long double)(c1_send_ts_)/1000000000;
-			#endif
-				
-				char line[4096];
-				string line_;
-				next_keyword = to_string(keywords[keyword_search_index++]);
-				printf("Searching for keyword: %s\n", next_keyword.c_str());
-
-				long nxt_kw = keywords[keyword_search_index-1];
-				double fw_ts_;
-				timestamp_table_t *tt_;
-				HASH_FIND(hh, timestamp_table, &nxt_kw, sizeof(long), tt_);
-				if(tt_ != NULL)
-					fw_ts_ = tt_->ts;
-				else
-					fw_ts_ = forward_ts;
-
-				pArg = PyTuple_New(3);
-				pValue = PyUnicode_FromString(next_keyword.c_str());
-				PyTuple_SetItem(pArg, 0, pValue);
-				string forward_ts_str = to_string(fw_ts_);
-				pValue = PyUnicode_FromString(forward_ts_str.c_str());
-				PyTuple_SetItem(pArg, 1, pValue);
-				pValue = PyUnicode_FromString(search_type);
-				PyTuple_SetItem(pArg, 2, pValue);
-				pValue = PyObject_CallObject(pFunc, pArg);
-				Py_DECREF(pArg);
-				if (pValue == NULL) {
-					PyErr_Print();
-					fprintf(stderr,"Call failed\n");
-					Py_XDECREF(pFunc);
-        			Py_DECREF(pModule);
-					return;
+		if(strlen(line) > max_log_len){
+			char t[4096], *ptr;
+			strcpy(t, line);
+			line[0] = '\0';
+			
+			ptr = strtok(t, ";");
+			while (ptr != NULL){
+				if(strlen(ptr)>220){
+					ptr[220]='\0';
 				}
-				stringstream ss;
-                ss << PyUnicode_AsUTF8(pValue);
-				Py_DECREF(pValue);
-					
-			#ifdef GET_STATS
-				// auto c1_recv_ts_ = chrono::system_clock::now().time_since_epoch().count();
-				// long double c1_recv_ts = (long double)(c1_recv_ts_)/1000000000;
-			#endif
-
-				while(getline(ss, line_, '\n')){
-					strcpy(line, line_.c_str());
-					#ifdef GET_STATS
-						total_log_lines++;
-					#endif
-					if (strtol(line, NULL, 10) == 0){
-						#ifdef GET_STATS
-						if (strncmp(line, "metainfo:", 9) == 0){
-							char *ptr = strtok(line, ":");
-							ptr = strtok(NULL, ":");
-							meta_data[meta_idx++] = stold(ptr);
-						}
-						#endif
-						continue;
-					}
-					char temp[4096], *ptr;
-					strcpy(temp, line);
-					ptr = strtok(temp, ";");
-					ptr = strtok(NULL, ";");
-					ptr = strtok(NULL, ";");
-					if (strncmp(ptr, " UBSI_ENTRY", 11)==0 || strncmp(ptr, " UBSI_EXIT", 10)==0 || strncmp(ptr, " UBSI_DEP", 9)==0)
-						continue;
-					ptr = strtok(ptr, "(");
-					ptr = strtok(NULL, ")");
-					int sysno = atoi(ptr);
-					// printf("sysno:%d, buf_add_index:%d\n", sysno, buf_add_index);
-					if (is_file_create(sysno)==0 && is_exec(sysno)==0 && is_write(sysno)==0 && is_read(sysno)==0 && is_fork_or_clone(sysno)==0)
-						continue;
-
-					if(strlen(line) > 500){
-						char t[4096], *ptr;
-						strcpy(t, line);
-						line[0] = '\0';
-						
-						ptr = strtok(t, ";");
-						while (ptr != NULL){
-							if(strlen(ptr)>220){
-								ptr[220]='\0';
-							}
-							strcat(line, ptr);
-							strcat(line, ";");
-							
-							ptr = strtok(NULL, ";");
-						}
-						line[strlen(line)]='\0';
-					}
-					strcpy(buf + (buf_add_index++ * max_log_len), line);
-				}
-				ss.str(string());
+				strcat(line, ptr);
+				strcat(line, ";");
 				
-				stop_index = buf_add_index-1;
-			#ifdef GET_STATS
-				// printf("\nIPC send time: %Lf", meta_data[0]-c1_send_ts);
-				// printf("\nIPC receive time: %Lf", c1_recv_ts-meta_data[1]);
+				ptr = strtok(NULL, ";");
+			}
+			line[strlen(line)]='\0';
+		}
 
-				lines_in_buf[count_call_to_server] = buf_add_index - start_index;
-				total_logs[count_call_to_server] = total_log_lines;
-			#endif
-				
-				debugtrack("Running for buf with index %d to %d\n", start_index, buf_add_index-1);
+		int flag=0, ret_pid=0, new_eid=1, l;	// flag is to denote if the event has been tainted.
+		long ret_inode=0;						// ret_inode and ret_pid is the pid/inode of tainted event.
+		long eid = strtol(temp, NULL, 10);
+		double ts = stod(temp+11);
 
-				for (k=start_index; k<=stop_index; k++){
-						int flag=0, ret_pid=0, new_eid=1, l;	// ret_inode and ret_pid is the pid/inode of tainted event.
-						long ret_inode=0;						// flag to denote if the event has been tainted.
-						char temp[500];
-						strcpy(temp, buf+k*max_log_len);
-						long eid = strtol(temp, NULL, 10);
-						double ts = stod(temp+11);
-
-						if (first_iteration){
-							long kw = keywords[keyword_search_index-1];
-							timestamp_table_t* tt;
-							HASH_FIND(hh, timestamp_table, &kw, sizeof(long), tt);
-							if (tt != NULL){
-								forward_ts = tt->ts;
-							}
-							else{
-								debugtrack("tt is null. %lf: %d\n", ts, kw);
-								forward_ts = ts;
-							}
-							first_iteration = 0;
-						}
-
-						debugtrack("\neid: %d, ts: %lf, forward_ts: %lf\t\t", eid, ts, forward_ts);
-						// for(l=0; l<eid_index; l++){
-						// 	if(eid_list[l]==eid){
-						// 		new_eid = 0;
-						// 		break;
-						// 	}
-						// }
-						
-						if(ts !=0 && ts >= forward_ts && new_eid == 1){
-							// printf("\nK: %d, %s\n", k, temp);
-							ft_syscall_handler(temp, ts, &forward_ts, &ret_pid, &ret_inode, &flag);
-							// eid_list[eid_index++] = (int)eid;
-
-							// extract pid and inode from the logs.
-							if (flag == 1){
-								debugtrack("\nret_pid: %d, ret_inode: %ld\n", ret_pid, ret_inode);
-								int pid_exist = 0, inode_exist = 0;
-								for (i=0; i<=keyword_add_index; i++){
-									if (ret_pid == keywords[i]){
-										pid_exist = 1;
-										break;
-									}
-								}
-								for (i=0; i<=keyword_add_index; i++){
-									if (ret_inode == keywords[i]){
-										inode_exist = 1;
-										break;
-									}
-								}
-								debugtrack("pid_exist %d, inode_exist %d\n", pid_exist, inode_exist);
-								if (pid_exist == 0 && ret_pid > 0)
-									keywords[++keyword_add_index] = long(ret_pid);
-								if (inode_exist == 0 && ret_inode > 0)
-									keywords[++keyword_add_index] = ret_inode;
-							}
-							// add a line to the new index only if the prev log is tainted, else replace it.
-							if (flag == 0)
-								buf_add_index--;
-						}
-				}
-				start_index = buf_add_index;
-				first_iteration = 1;
-
-				debugtrack("\nnext keyword: %ld\n", keywords[keyword_search_index]);
-				debugtrack("lines in buf: %d\n", buf_add_index);
-
-			#ifdef GET_STATS
-				auto loop_end_ts = chrono::system_clock::now();
-				runtime[count_call_to_server++] = loop_end_ts-loop_start_ts;
-			#endif
-		} while (keywords[keyword_search_index] != 0);
-
-		Py_XDECREF(pFunc);
-		Py_DECREF(pModule);
-		if (Py_FinalizeEx() < 0)
-			return;
-	
-		#ifdef GET_STATS
-			printf("\nTotal calls to server: %d\n", count_call_to_server);
-			printf("\nkeyword\t#total_logs\t#relevant_logs\truntime\n");
-			for (k=0; k<count_call_to_server; k++)
-				printf("%ld\t%d\t%d\t%lf\n", keywords[k], total_logs[k], lines_in_buf[k], runtime[k].count());
-		#endif
-	// }
+		if (first_iteration){
+			timestamp_table_t* tt;
+			HASH_FIND(hh, timestamp_table, &kw, sizeof(long), tt);
+			if (tt != NULL){
+				debugtrack("tt is not null. %lf: %d\n", tt->ts, kw);
+				forward_ts = tt->ts;
+			}
+			else{
+				debugtrack("tt is null. %lf: %d\n", ts, kw);
+				forward_ts = ts;
+			}
+			first_iteration = 0;
+		}
+		ft_syscall_handler(line, ts, &forward_ts, &ret_pid, &ret_inode, &flag);
+    }
 }
 
 
@@ -504,7 +334,6 @@ int main(int argc, char** argv)
 					printf("Usage: ./UBSI_bt [-i log_file] [-t init_table] [-f file_inode] [-p process_pid]\n");
 					return 0;
 			}
-			fclose(fp);
 		}
 		
 		if(log_name != NULL && init_table_name == NULL) {
@@ -524,12 +353,6 @@ int main(int argc, char** argv)
 				save_init_tables(init_table_name);
 		}
 		
-		if(log_name != NULL){
-				fp = fopen(log_name, "r");
-				generate_fp_table(fp);
-				print_fp_table();
-		}
-		
 		if(user_pid > 0) {
 				printf("user taint tid = %d, pid = %d\n", user_pid, get_pid(user_pid));
 				taint_all_units_in_pid(user_pid, "start_node");
@@ -544,13 +367,9 @@ int main(int argc, char** argv)
 				if (path[0] == ' ') path = path.substr(1);
 				taint_inode(user_inode, user_eid, path);
 		}
-		if(log_name != NULL){
-				fclose(fp);
-		}
-		else{
-				printf("calling table scan.\n");
-				table_scan_f(user_pid, user_inode);
-		}
+
+		table_scan_f(user_pid, user_inode, fp);
+		fclose(fp);
 
 		fp = fopen("AUDIT_ft.gv", "w");
 
